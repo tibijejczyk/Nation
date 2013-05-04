4e92a6a98ddb7b906aa3c9836dc9a94ae59a7c09
DeX3r <dex3r.dx@gmail.com>
2013-05-04 20:39:24 +0300

    Druga klasa (II c)

--- src/pl/nation/NationMAIN.java
+++ src/pl/nation/NationMAIN.java
@@ -3,4 +3,13 @@ package pl.nation;
 public class NationMAIN {
 
 //Nation GameMod.
+//Trololololololololo
+
+  public void main(String[] args)
+  {
+      while(true)
+      {
+        System.out.println("Oskar: Dex moim Panem!");
+      }
+  }
 }
